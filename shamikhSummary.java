e61324ba4f4dbd1f67beb53f355cba62b33b2afd
shamikhquraan1 <shamikhq@yahoo.com>
2021-12-06 02:06:03 +0200

    adidng Functions

--- shamikhSummary.java
+++ shamikhSummary.java
@@ -343,3 +343,47 @@ public int findSecondMinimumValue(TreeNode root) {
       }
       return list;
   }
+
+
+
+
+
+
+
+ //Add ToBinarySearchTree
+    private Node addToTree(Node current, int value) {
+        if (current == null) {
+            return new Node(value);
+        }
+        if ( value < (int)current.value ) {
+            current.left = addToTree(current.left, value);
+        } else if (value > (int) current.value) {
+            current.right = addToTree(current.right, value);
+        } else {
+            return current;
+        }
+        return current;
+    }
+
+
+
+
+
+    // Insert TO BinarySearchTree
+    public void insert(int value) {
+        root = addToTree(root, value);
+    }
+    private boolean containsNode(Node current, int value) {
+        if (current == null) {
+            return false;
+        }
+        if (value ==(int) current.value) {
+            return true;
+        }
+        return value <(int) current.value
+                ? containsNode(current.left, value)
+                : containsNode(current.right, value);
+    }
+    public boolean contain(int value) {
+        return containsNode(root, value);
+    }
